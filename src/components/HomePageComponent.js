6891636d14a3ed7fcf6849198439e9ffa17538ce
Lord Mendoza <lord.d.mendoza@gmail.com>
2024-08-29 12:20:14 -0400

    Initial commit

---
+++ src/components/HomePageComponent.js
@@ -0,0 +1,11 @@
+import {Component} from "react";
+
+class HomePageComponent extends Component {
+    render() {
+        return <div>
+            Hi there!
+        </div>
+    }
+}
+
+export default HomePageComponent;

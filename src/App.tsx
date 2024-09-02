ce50785cda533f8356f68d21da2991e48ea873bf
Andy Tran <shiro@mail.shir.one>
2024-09-02 00:56:58 -0400

    Added AIChatBot.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import HelloWorld from "./components/HelloWorld";
 import ListGroup from "./components/ListGroup";
 import DefaultNoteFile from "./components/DefaultNoteFile";
 import EditNote from "./components/EditNote";
+import AIChatBot from "./components/AIChatBot";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <ListGroup></ListGroup>
       <DefaultNoteFile></DefaultNoteFile>
       <EditNote></EditNote>
+      <AIChatBot></AIChatBot>
     </div>
   );
 }

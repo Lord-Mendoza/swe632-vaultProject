5046014ee835a129d3e04684b1faf2c3c4bd9810
Andy Tran <45242077+Ahtran360@users.noreply.github.com>
2024-11-13 11:58:08 -0500

    CTRL+S Implemented

--- src/components/EntryComponent.js
+++ src/components/EntryComponent.js
@@ -1,133 +1,199 @@
-import {Form, Modal} from "react-bootstrap";
-import {ConstantStrings} from "../utilities/constants/ConstantStrings";
-import {Button} from "semantic-ui-react";
-import {Component} from "react";
-import {copyObject, isNotAnEmptyObject} from "../utilities/helpers/ObjectVariableFunctions";
-import {isNotEmptyString} from "../utilities/helpers/StringVariableValidators";
+import { Form, Modal } from "react-bootstrap";
+import { ConstantStrings } from "../utilities/constants/ConstantStrings";
+import { Button, Icon, Menu, Segment, Sidebar } from "semantic-ui-react";
+import { Component } from "react";
+import {
+  copyObject,
+  isNotAnEmptyObject,
+} from "../utilities/helpers/ObjectVariableFunctions";
+import { isNotEmptyString } from "../utilities/helpers/StringVariableValidators";
+import { Toast, ToastContainer } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styling/SuccessToast.css";
 
 class EntryComponent extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        let entry = {};
-        if (isNotAnEmptyObject(this.props["entry"])) {
-            Object.keys(this.props["entry"]).forEach(prop => {
-                if (prop === "isCode")
-                    entry["isCode"] = this.props["entry"][prop];
-                else
-                    entry[prop] = this.props["entry"][prop];
-            })
-        }
-
-        this.state = {
-            entry,
-            originalEntry: copyObject(entry)
-        }
-
-        this.handleFormInput = this.handleFormInput.bind(this);
-        this.validateFields = this.validateFields.bind(this);
-        this.showRefreshConfirmation = this.showRefreshConfirmation.bind(this);
+    let entry = {};
+    if (isNotAnEmptyObject(this.props["entry"])) {
+      Object.keys(this.props["entry"]).forEach((prop) => {
+        if (prop === "isCode") entry["isCode"] = this.props["entry"][prop];
+        else entry[prop] = this.props["entry"][prop];
+      });
     }
 
-    componentDidMount() {
-        // Add event listener when the modal is open
-        window.addEventListener('beforeunload', this.showRefreshConfirmation);
-    }
-
-    componentWillUnmount() {
-        // Remove event listener when the modal is closed
-        window.removeEventListener('beforeunload', this.showRefreshConfirmation);
-    }
-
-    showRefreshConfirmation(event) {
-        // Custom confirmation message (browsers may not display it directly)
-        const message = "";
-        event.returnValue = "Are you sure you want to refresh? Any unsaved data may be lost.";
-        return message; // For some browsers like older versions of Firefox
+    this.state = {
+      entry,
+      originalEntry: copyObject(entry),
     };
 
+    this.handleFormInput = this.handleFormInput.bind(this);
+    this.validateFields = this.validateFields.bind(this);
+    this.showRefreshConfirmation = this.showRefreshConfirmation.bind(this);
+  }
 
-    handleFormInput(e) {
-        const {entry} = this.state;
-        const {name, value, checked} = e.target;
+  componentDidMount() {
+    // Add event listener when the modal is open
+    window.addEventListener("beforeunload", this.showRefreshConfirmation);
+  }
 
-        let newEntry = copyObject(entry);
-        if (name === "isCode")
-            newEntry[name] = checked;
-        else
-            newEntry[name] = value;
+  componentWillUnmount() {
+    // Remove event listener when the modal is closed
+    window.removeEventListener("beforeunload", this.showRefreshConfirmation);
+  }
 
-        this.setState({entry: newEntry});
+  showRefreshConfirmation(event) {
+    // Custom confirmation message (browsers may not display it directly)
+    const message = "";
+    event.returnValue =
+      "Are you sure you want to refresh? Any unsaved data may be lost.";
+    return message; // For some browsers like older versions of Firefox
+  }
+
+  handleFormInput(e) {
+    const { entry } = this.state;
+    const { name, value, checked } = e.target;
+
+    let newEntry = copyObject(entry);
+    if (name === "isCode") newEntry[name] = checked;
+    else newEntry[name] = value;
+
+    this.setState({ entry: newEntry });
+  }
+
+  validateFields() {
+    const { entry } = this.state;
+    const { title, description } = entry;
+
+    if (isNotEmptyString(title) && isNotEmptyString(description)) {
+      this.props["changeEntries"](entry);
+
+    } else {
+      alert("Please fill in all required fields before submitting.");
     }
+  }
 
-    validateFields() {
-        const {entry} = this.state;
-        const {title, description} = entry;
-
-        if (isNotEmptyString(title) && isNotEmptyString(description)) {
-            this.props["changeEntries"](entry)
-        } else {
-            alert("Please fill in all required fields before submitting.")
-        }
+  // Needed for Ctrl+S
+  componentDidMount() {
+    // Bind the global keydown event
+    document.addEventListener("keydown", this.handleSaveShortcut);
+  }
+  // Needed for Ctrl+S
+  componentWillUnmount() {
+    // Unbind the global keydown event
+    document.removeEventListener("keydown", this.handleSaveShortcut);
+  }
+  // Needed for Ctrl+S
+  // Must invoke here, since in the return, it will invoke the browser's shorcut
+  handleSaveShortcut = (e) => {
+    if (e.key === "s" && e.ctrlKey) {
+      e.preventDefault(); // Prevent the browser's Ctrl+S default action
+      this.validateFields(); // Trigger the save action
     }
+  };
 
-    render() {
-        const {entry, originalEntry} = this.state;
 
-        return <Modal show={true} onHide={this.props["closePopup"]}
-                      backdrop={"static"} className={"entryPopup"}
-                      aria-labelledby="contained-modal-title-vcenter" centered>
-            <Modal.Header closeButton>
-                <Modal.Title><b>{this.props["entryType"] === ConstantStrings.editStr ? "Edit Entry" : "Create New Entry"}</b></Modal.Title>
-            </Modal.Header>
+  render() {
+    const { entry, originalEntry } = this.state;
 
-            <Modal.Body>
-                <Form>
-                    <Form.Group className="mb-3" controlId="title">
-                        <Form.Label><b><span style={{color: '#db2828'}}>*</span></b> Title</Form.Label>
-                        <Form.Control name="title" type="text" placeholder="Title"
-                                      onChange={this.handleFormInput}
-                                      value={entry["title"]}
-                        />
-                    </Form.Group>
+    return (
+      <Modal
+        show={true}
+        onHide={this.props["closePopup"]}
+        backdrop={"static"}
+        className={"entryPopup"}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <b>
+              {this.props["entryType"] === ConstantStrings.editStr
+                ? "Edit Entry"
+                : "Create New Entry"}
+            </b>
+          </Modal.Title>
+        </Modal.Header>
 
-                    <Form.Group className="mb-3" controlId="description">
-                        <Form.Label><b><span style={{color: '#db2828'}}>*</span></b> Description</Form.Label>
-                        <Form.Control name="description" as="textarea" placeholder="Description"
-                                      onChange={this.handleFormInput}
-                                      value={entry["description"]}
-                                      style={{minHeight: "150px"}}
-                        />
-                        <Form.Check name="isCode" type="checkbox" label="Format as code?"
-                                    onChange={this.handleFormInput}
-                                    checked={entry["isCode"]}
-                                    style={{paddingTop: "10px"}}
-                        />
-                    </Form.Group>
-                </Form>
-            </Modal.Body>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="title">
+              <Form.Label>
+                <b>
+                  <span style={{ color: "#db2828" }}>*</span>
+                </b>{" "}
+                Title
+              </Form.Label>
+              <Form.Control
+                name="title"
+                type="text"
+                placeholder="Title"
+                onChange={this.handleFormInput}
+                value={entry["title"]}
+              />
+            </Form.Group>
 
-            <Modal.Footer>
-                <Button color="red" onClick={this.props["closePopup"]} style={{float: "left"}}>
-                    Cancel
-                </Button>
+            <Form.Group className="mb-3" controlId="description">
+              <Form.Label>
+                <b>
+                  <span style={{ color: "#db2828" }}>*</span>
+                </b>{" "}
+                Description
+              </Form.Label>
+              <Form.Control
+                name="description"
+                as="textarea"
+                placeholder="Description"
+                onChange={this.handleFormInput}
+                value={entry["description"]}
+                style={{ minHeight: "150px" }}
+              />
+              <Form.Check
+                name="isCode"
+                type="checkbox"
+                label="Format as code?"
+                onChange={this.handleFormInput}
+                checked={entry["isCode"]}
+                style={{ paddingTop: "10px" }}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
 
-                {
-                    this.props["entryType"] === ConstantStrings.editStr &&
-                    <Button onClick={() => {
-                        if (window.confirm("Are you sure you want to revert your changes? This cannot be undone."))
-                            this.setState({entry: originalEntry})
-                    }}>
-                        Undo Changes
-                    </Button>
-                }
+        <Modal.Footer>
+          <Button
+            color="red"
+            onClick={this.props["closePopup"]}
+            style={{ float: "left" }}
+          >
+            Cancel
+          </Button>
 
-                <Button color="green" onClick={this.validateFields}>
-                    Save Changes
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    }
+          {this.props["entryType"] === ConstantStrings.editStr && (
+            <Button
+              onClick={() => {
+                if (
+                  window.confirm(
+                    "Are you sure you want to revert your changes? This cannot be undone."
+                  )
+                )
+                  this.setState({ entry: originalEntry });
+              }}
+            >
+              Undo Changes
+            </Button>
+          )}
+
+          <Button color="green" onClick={this.validateFields}>
+            {/* For Ctrl+S save implementation, see "handleSaveShorcut function*/}
+            Save Changes
+          </Button>
+         
+        </Modal.Footer>
+      </Modal>
+    );
+  }
 }
 
 export default EntryComponent;
